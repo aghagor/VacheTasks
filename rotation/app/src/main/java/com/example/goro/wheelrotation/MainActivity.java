e646daaf9527f39a13f951eb74f3a80a3500a7e7
aghagor@gmail.com <aghagor@gmail.com>
2018-02-16 20:33:09 +0400

    finishing this app

--- rotation/app/src/main/java/com/example/goro/wheelrotation/MainActivity.java
+++ rotation/app/src/main/java/com/example/goro/wheelrotation/MainActivity.java
@@ -1,13 +1,70 @@
 package com.example.goro.wheelrotation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+    private ImageView wheel;
+    private double mCurrAngle = 0;
+    private double mPrevAngle = 0;
+    private TextView degree;
+    private String mmCurrAngle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wheel = findViewById(R.id.imageView1);
+        degree = findViewById(R.id.tv_degree);
+
+        wheel.setOnTouchListener(this);
+
+    }
+
+    @Override
+    public boolean onTouch(final View v, MotionEvent event) {
+        final float xc = wheel.getWidth() / 2;
+        final float yc = wheel.getHeight() / 2;
+
+        final float x = event.getX();
+        final float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                wheel.clearAnimation();
+                mCurrAngle = Math.toDegrees(Math.atan2(x - xc, yc - y));
+                break;
+            }
+            case MotionEvent.ACTION_MOVE: {
+                mPrevAngle = mCurrAngle;
+                mCurrAngle = Math.toDegrees(Math.atan2(x - xc, yc - y));
+                animate(mPrevAngle, mCurrAngle, 0);
+                break;
+
+            }
+            case MotionEvent.ACTION_UP: {
+                mPrevAngle = mCurrAngle = 0;
+                break;
+            }
+        }
+        return true;
+    }
+
+    private void animate(double fromDegrees, double toDegrees, long durationMillis) {
+        final RotateAnimation rotate = new RotateAnimation((float) fromDegrees, (float) toDegrees,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+                RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(durationMillis);
+        rotate.setFillEnabled(true);
+        rotate.setFillAfter(true);
+        wheel.startAnimation(rotate);
+        mmCurrAngle = String.format("%.2f", mCurrAngle);
+        degree.setText("The rotation degree is " + mmCurrAngle);
     }
 }
