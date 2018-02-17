7b6bea13583dde9625b4d532282ca3a95cea5773
aghagor@gmail.com <aghagor@gmail.com>
2018-02-17 16:32:12 +0400

    correcting all problams

--- rotation/app/src/main/java/com/example/goro/wheelrotation/MainActivity.java
+++ rotation/app/src/main/java/com/example/goro/wheelrotation/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.goro.wheelrotation;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+public class MainActivity extends AppCompatActivity {
+
+    private GestureDetector mDetector;
+    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
     private ImageView wheel;
     private double mCurrAngle = 0;
     private double mPrevAngle = 0;
@@ -23,38 +30,17 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         wheel = findViewById(R.id.imageView1);
         degree = findViewById(R.id.tv_degree);
 
-        wheel.setOnTouchListener(this);
-
+        mDetector = new GestureDetector(this, new MyGestureListener());
+        wheel.setOnTouchListener(touchListener);
     }
 
-    @Override
-    public boolean onTouch(final View v, MotionEvent event) {
-        final float xc = wheel.getWidth() / 2;
-        final float yc = wheel.getHeight() / 2;
+    View.OnTouchListener touchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            return mDetector.onTouchEvent(event);
 
-        final float x = event.getX();
-        final float y = event.getY();
-
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN: {
-                wheel.clearAnimation();
-                mCurrAngle = Math.toDegrees(Math.atan2(x - xc, yc - y));
-                break;
-            }
-            case MotionEvent.ACTION_MOVE: {
-                mPrevAngle = mCurrAngle;
-                mCurrAngle = Math.toDegrees(Math.atan2(x - xc, yc - y));
-                animate(mPrevAngle, mCurrAngle, 0);
-                break;
-
-            }
-            case MotionEvent.ACTION_UP: {
-                mPrevAngle = mCurrAngle = 0;
-                break;
-            }
         }
-        return true;
-    }
+    };
 
     private void animate(double fromDegrees, double toDegrees, long durationMillis) {
         final RotateAnimation rotate = new RotateAnimation((float) fromDegrees, (float) toDegrees,
@@ -66,5 +52,99 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         wheel.startAnimation(rotate);
         mmCurrAngle = String.format("%.2f", mCurrAngle);
         degree.setText("The rotation degree is " + mmCurrAngle);
+
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener implements View.OnTouchListener {
+
+        @Override
+        public boolean onTouch(final View v, MotionEvent event) {
+            final float xc = wheel.getWidth() / 2;
+            final float yc = wheel.getHeight() / 2;
+
+            final float x = event.getX();
+            final float y = event.getY();
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN: {
+                    wheel.clearAnimation();
+                    mCurrAngle = Math.toDegrees(Math.atan2(x - xc, yc - y));
+                    break;
+                }
+                case MotionEvent.ACTION_MOVE: {
+                    mPrevAngle = mCurrAngle;
+                    mCurrAngle = Math.toDegrees(Math.atan2(x - xc, yc - y));
+                    animate(mPrevAngle, mCurrAngle, 0);
+                    break;
+
+                }
+                case MotionEvent.ACTION_UP: {
+                    mPrevAngle = mCurrAngle = 0;
+                    break;
+                }
+            }
+            return true;
+        }
+        @Override
+        public boolean onDown(MotionEvent event) {
+            Log.d("TAG", "onDown: ");
+
+            return true;
+        }
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            Log.i("TAG", "onSingleTapConfirmed: ");
+            return true;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            Log.i("TAG", "onLongPress: ");
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            Log.i("TAG", "onDoubleTap: ");
+            return true;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                                float distanceX, float distanceY) {
+            Log.i("TAG", "onScroll: ");
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2,
+                               float velocityX, float velocityY) {
+            final float xc = wheel.getWidth() / 2;
+            final float yc = wheel.getHeight() / 2;
+
+            final float x = e1.getX();
+            final float y = e1.getY();
+
+            Log.d("TAG", "onFling: ");
+            if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                mPrevAngle = mCurrAngle;
+                mCurrAngle = Math.toDegrees(Math.atan2(x - xc, yc - y));
+                animate(mPrevAngle, mCurrAngle, 0);
+                return false; // Right to left
+            } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                mPrevAngle = mCurrAngle;
+                mCurrAngle = Math.toDegrees(Math.atan2(x - xc, yc - y));
+                animate(mPrevAngle, mCurrAngle, 0);
+                return false; // Left to right
+            }
+
+            if (e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                return false; // Bottom to top
+            } else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+                return false; // Top to bottom
+            }
+            return false;
+        }
+
     }
 }
